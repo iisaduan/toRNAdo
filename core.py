9808988be55eaaf0042102154a21ee49466c2cac
JingyiRose <roliu@g.hmc.edu>
2022-07-29 16:24:53 -0700

    add short names for command line options

--- core.py
+++ core.py
@@ -7,7 +7,7 @@ from algs.nussinov import *
 PLOT_FILENAME = "plot"
 
 def run_Zuker_functions(rna: str, folding: list = None, internal_loop_size: int = None,\
-    d: bool = False, h: bool = False, plot_file: str = PLOT_FILENAME):
+    display: bool = False, plot: bool = False, plot_file: str = PLOT_FILENAME):
 
     print("-----Running Zuker's algorithm-----")
     # run Zuker algorithm to get optimal folds
@@ -38,19 +38,19 @@ def run_Zuker_functions(rna: str, folding: list = None, internal_loop_size: int 
     print("The distance vector is: ", distance_vector)
     num_folds_dp = reduce((lambda x, y: x + y), distance_vector)
     print("The number of optimal folds is: ", num_folds_dp)
-    if d:
+    if display:
         # display an optimal fold that is the most distant from the given fold
         max_distance_fold = distance_solver.get_one_max_dist_solution()
         print("One of the farthest foldings is: ")
         print(convert_folding_to_DBN(max_distance_fold))
-    if h:
-        # display the histogram for the distance vector
+    if plot:
+        # plot the histogram for the distance vector
         display_histogram(distance_vector, 'Z', plot_file, folding_given)
     print()
 
 
-def run_Nussinov_functions(rna: str, folding: list = None, d: bool = False,\
-    h: bool = False, plot_file: str = PLOT_FILENAME):
+def run_Nussinov_functions(rna: str, folding: list = None, display: bool = False,\
+    plot: bool = False, plot_file: str = PLOT_FILENAME):
     print("-----Running Nussinov's algorithm-----")
     # run Nussinov algorithm to get optimal folds
     opt_val, opt_solutions = nussinov_dp(rna)
@@ -73,19 +73,19 @@ def run_Nussinov_functions(rna: str, folding: list = None, d: bool = False,\
     print("The distance vector is: ", distance_vector)
     num_folds_dp = reduce((lambda x, y: x + y), distance_vector)
     print("The number of optimal folds is: ", num_folds_dp)
-    if d:
+    if display:
         # display an optimal fold that is the most distant from the given fold
         max_distance_fold = construct_one_opt_solution(rna, max_distance_solutions)
         print("One of the farthest foldings is: ")
         print(convert_folding_to_DBN(max_distance_fold))
-    if h:
-        # display the histogram for the distance vector
+    if plot:
+        # plot the histogram for the distance vector
         display_histogram(distance_vector, 'N', plot_file, folding_given)
     print()
 
 
 if __name__ == '__main__':
     rna = "CUUCCCAGGUAACAAACCAACCAACUUUCGAUCUCUUGUAGAUCUGUUCUCUAAACGCUUCCCAGGUAACAAACCAACCAACUUUCGAUCUCUUGUAGAUCUGUUCUCUAAACG"
-    run_Nussinov_functions(rna, d=True, h=True, plot_file="plot_Nussinov")
-    run_Zuker_functions(rna, d=True, h=True, plot_file="plot_Zuker", internal_loop_size=None)
-    run_Zuker_functions(rna, d=True, h=True, plot_file="plot_Zuker_30", internal_loop_size=30)
+    run_Nussinov_functions(rna, display=True, plot=True, plot_file="plot_Nussinov")
+    run_Zuker_functions(rna, display=True, plot=True, plot_file="plot_Zuker", internal_loop_size=None)
+    run_Zuker_functions(rna, display=True, plot=True, plot_file="plot_Zuker_30", internal_loop_size=30)
